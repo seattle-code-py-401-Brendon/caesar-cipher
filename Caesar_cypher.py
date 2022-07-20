97da8cf8ef1eef13e235a1e6f5eb1839c4779d02
Brendon <brendonhampton1988@gmail.com>
2022-07-19 21:48:04 -0700

    got a shift working but not as intended

---
+++ Caesar_cypher.py
@@ -0,0 +1,41 @@
+import string
+
+"""
+save a list of the alphabet
+create a function called encrypt
+      takes in two parameters
+        phrase(string), num(shift)
+the phrase will then be shifted that many letters of the alphabet
+shifts that exceed 26 should wrap around.
+shifts that push a letter out or range should wrap around.
+"""
+
+"""
+Create a function called decrypt
+    takes in the encrypted message and performs the encrypt but with the negative shift
+    
+    
+Create a crack method that brute forces through all the shifts 
+
+   
+"""
+# create alphabet
+alphabet = list(string.ascii_lowercase)
+for i in range(len(alphabet)):
+    print(i, alphabet[i])
+
+# create encrypt function
+def encrypt(phrase, shift=None):
+    shifted_phrase = ''
+    # get the index location for alphabet
+    for i in range(len(alphabet)):
+        for letter in phrase:
+            if alphabet[i] == letter:
+                letter = alphabet[i + shift]
+                shifted_phrase += letter
+    print(shifted_phrase)
+
+
+if __name__ == "__main__":
+    phrase_one = 'ax by cgc dkd'
+    encrypt(phrase_one, 1)
